77d734228421f0ac99a1dbdab0699ff27b1148cf
Thomas Freeman <tomclaudefreeman@gmail.com>
2020-02-05 16:55:40 +1100

    Add comments

--- bubble.py
+++ bubble.py
@@ -1,9 +1,14 @@
 from utils.shared import swap_elements
 from utils.validator import is_sorted
+from copy import deepcopy
 
 
 def bubble_sort(array):
-    while not is_sorted(array):
-        for i in range(len(array) - 1):
-            if array[i] > array[i + 1]:
-                swap_elements(array, i, i + 1)
+    # A copy of the original array is made, so that any modifications
+    # do not affect it
+    working_arr = deepcopy(array)
+    while not is_sorted(working_arr):
+        for i in range(len(working_arr) - 1):
+            if working_arr[i] > working_arr[i + 1]:
+                swap_elements(working_arr, i, i + 1)
+    return working_arr
--- insertion.py
+++ insertion.py
@@ -1,13 +1,17 @@
+# Places a given number into its correct (sorted) position in an array
 def insert_element(num, array):
+    # Quick edge case for optimisation
+    if num < min(array):
+        array.insert(0, num)
+    # Loops through the list in reverse order
     for i, val in reversed(list(enumerate(array))):
         if num > val:
             array.insert(i + 1, num)
             return
-    array.insert(0, num)
 
 
 def insertion_sort(array: list):
-    subarray = [array[0]]
+    subarray = [array[0]] # Stores all sorted items
     for num in array[1:]:
         insert_element(num, subarray)
     return subarray
